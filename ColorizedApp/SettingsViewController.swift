d704514435711cb008fcdfca0a9a01fb88998454
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-07-13 18:12:16 +0300

    Version with Done button, but just left

--- ColorizedApp/SettingsViewController.swift
+++ ColorizedApp/SettingsViewController.swift
@@ -20,6 +20,12 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
     
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
+    @IBOutlet var toolbar: UIToolbar!
+    
     var uiColor: UIColor!
     var delegate: SettingsViewControllerDelegate!
     
@@ -28,32 +34,51 @@ class SettingsViewController: UIViewController {
         
         colorView.layer.cornerRadius = 20
         
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
+        
         redSlider.value = Float(uiColor.components.red)
         greenSlider.value = Float(uiColor.components.green)
         blueSlider.value = Float(uiColor.components.blue)
         
         setColor()
         setValue(for: redLabel, greenLabel, blueLabel)
+        setValueOfTextFiels()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     // MARK: - IB Actions
-    
     @IBAction func sliderAction(_ sender: UISlider) {
         setColor()
         switch sender {
         case redSlider:
             redLabel.text = string(from: redSlider)
-        case greenLabel:
+            redTextField.text = string(from: redSlider)
+        case greenSlider:
             greenLabel.text = string(from: greenSlider)
+            greenTextField.text = string(from: greenSlider)
         default:
             blueLabel.text = string(from: blueSlider)
+            blueTextField.text = string(from: blueSlider)
         }
     }
     
     @IBAction func doneButtonPressed() {
-        delegate.setViewColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value ), blue: CGFloat(blueSlider.value))
+        delegate.setViewColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value ),
+            blue: CGFloat(blueSlider.value)
+        )
         dismiss(animated: true)
-        
+    }
+    
+    @IBAction func toolbarDoneButtonTapped(_ sender: UIBarButtonItem) {
+        view.endEditing(true)
     }
     
     // MARK: - Private Mathods
@@ -83,6 +108,12 @@ class SettingsViewController: UIViewController {
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
+    
+    private func setValueOfTextFiels() {
+        redTextField.text = string(from: redSlider)
+        greenTextField.text = string(from: greenSlider)
+        blueTextField.text = string(from: blueSlider)
+    }
 }
 
 // MARK: UIColor
@@ -93,6 +124,32 @@ extension UIColor {
     var components: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
         let coreImageColor = self.coreImageColor
         return (coreImageColor.red, coreImageColor.green, coreImageColor.blue, coreImageColor.alpha)
+    }
+}
+
+// MARK: UITextFieldDelegate
+extension SettingsViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let newValue = textField.text else { return }
+        guard let numberValue = Float(newValue) else { return }
+        
+        if textField == redTextField {
+            redLabel.text = newValue
+            redSlider.value = numberValue
+        } else if textField == greenTextField {
+            greenLabel.text = newValue
+            greenSlider.value = numberValue
+        } else {
+            blueLabel.text = newValue
+            blueSlider.value = numberValue
+        }
+        setColor()
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        textField.inputAccessoryView = toolbar
+        return true
     }
 }
 
